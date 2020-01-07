ef267cd879cdcf35aec0ecd701c6c75c7ea8f63c
Philip Fischbacher <pfischbacher@gmail.com>
2020-01-07 21:04:02 +0900

    added test module, test.js and updated index.js file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var http = require('http')
 var math = require('./modules/add')
+var test = require('./modules/test')
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
   var sum = math.add(1, 2)
   res.write('SUM =' + sum)
+  res.write('\r\n' + test.test())
   res.write('\r\nHello World!')
   res.end()
 }).listen(8080)
